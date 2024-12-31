f12b72de1686288b19f5f2faf3d8dba8bb9019c9
dx1ded <doskovol1@gmail.com>
2024-12-30 18:15:29 -0600

    Modified page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <button type="button" data-testid="show-result">
         Show result
       </button>
-      <div>Hello World!!!!!s</div>
+      <div>Hello World!!!!!2s</div>
     </div>
   )
 }
