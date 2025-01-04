b651fe543fbd19769a57f27ecd1819768dd67b46
dx1ded <doskovol1@gmail.com>
2025-01-03 23:33:18 -0600

    Changed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <button type="button" data-testid="show-result">
         Show result
       </button>
-      <div>Some other shi</div>
+      <div>New release</div>
     </div>
   )
 }
