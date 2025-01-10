48471626b1fb0d711c30936c860693faa6a12f10
dx1ded <doskovol1@gmail.com>
2025-01-09 18:40:07 -0600

    Change div text in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <button type="button" data-testid="show-result">
         Show result
       </button>
-      <div>New release! 1</div>
+      <div>New release! Whatever</div>
     </div>
   )
 }
